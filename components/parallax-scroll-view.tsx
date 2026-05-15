cbec0062b5002e3827fd207a98136ae0273d5b2e
juliaism <jjatene30@gmail.com>
2026-05-14 23:33:31 -0300

    ajustes

--- components/parallax-scroll-view.tsx
+++ components/parallax-scroll-view.tsx
@@ -1,15 +1,10 @@
 import type { PropsWithChildren, ReactElement } from 'react';
 import { StyleSheet } from 'react-native';
-import Animated, {
-  interpolate,
-  useAnimatedRef,
-  useAnimatedStyle,
-  useScrollOffset,
-} from 'react-native-reanimated';
+import Animated, {interpolate, useAnimatedRef, useAnimatedStyle, useScrollOffset} from 'react-native-reanimated';
+import { ThemedView } from './themed-view';
+import { useThemeColor } from '../hooks/use-theme-color';
+import { useColorScheme } from 'react-native';
 
-import { ThemedView } from '@/components/themed-view';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-import { useThemeColor } from '@/hooks/use-theme-color';
 
 const HEADER_HEIGHT = 250;
 
