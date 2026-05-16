3be4d5ac6873332c1a2b6b7a0b14d9c5da35784f
juliaism <jjatene30@gmail.com>
2026-05-15 21:16:11 -0300

    update

--- app/src/service/authService.ts
+++ app/src/service/authService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000"; // troque pelo IP da sua máquina
+const API_URL = "http://localhost:3000"; 
 
 export const signup = async (phone: string, password: string) => {
   const response = await axios.post(`${API_URL}/auth/signup`, { phone, password });
